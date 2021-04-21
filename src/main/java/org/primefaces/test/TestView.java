58412759c0e5a811e74e63c6767d6a8cdd94b05e
Jonas Chojnicki <jonas.chojnicki@materna.de>
2021-04-21 15:00:01 +0200

    Copy of SelectCheckbox-Showcase with emptyLabel

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -3,6 +3,8 @@ package org.primefaces.test;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
@@ -18,10 +20,24 @@ public class TestView implements Serializable {
     private Integer integer;
     private BigDecimal decimal;
     private LocalDateTime localDateTime;
+
+    private String[] selectedCities;
+    private List<String> cities;
     
     @PostConstruct  
     public void init() {
-        string = "Welcome to PrimeFaces!!!";
+        string = "Welcome to PrimeFaces emptyLabel Test!!!";
+
+        cities = new ArrayList<>();
+        cities.add("Miami");
+        cities.add("London");
+        cities.add("Paris");
+        cities.add("Istanbul");
+        cities.add("Berlin");
+        cities.add("Barcelona");
+        cities.add("Rome");
+        cities.add("Brasilia");
+        cities.add("Amsterdam");
     }
 
 }
